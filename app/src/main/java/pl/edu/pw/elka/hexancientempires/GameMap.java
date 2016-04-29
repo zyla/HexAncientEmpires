d199da7b2369c2e12cad93dfad4e66c1f620368f
Only1Loatheb <zielinski.x360@interia.pl>
2016-04-29 20:46:29 +0200

    adding empty class //doin nuttin

---
+++ app/src/main/java/pl/edu/pw/elka/hexancientempires/GameMap.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.elka.hexancientempires;
+
+/**
+ * Class that represents board
+ * Created by tomek on 29.04.16.
+ * I have no idea what I am doing lol
+ */
+public class GameMap {
+    class Hexagon
+    {
+
+    }
+}
