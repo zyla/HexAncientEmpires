2547560f22de31a19f359f1699882bf8bf3326df
Maciej Bielecki <zyla@prati.pl>
2016-04-30 14:12:15 +0200

    Add a simple hexagon drawing demo

--- app/src/main/java/pl/edu/pw/elka/hexancientempires/MainActivity.java
+++ app/src/main/java/pl/edu/pw/elka/hexancientempires/MainActivity.java
@@ -2,13 +2,19 @@ package pl.edu.pw.elka.hexancientempires;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //random comment
+
+        gameView = new GameView(this);
+        FrameLayout layout = new FrameLayout(this);
+        layout.addView(gameView);
+        setContentView(layout);
     }
 }
