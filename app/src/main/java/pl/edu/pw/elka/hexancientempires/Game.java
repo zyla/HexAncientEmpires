af8ebfa0cf83786f0c7ca05fa472de3ea20f8f3f
Only1Loatheb <zielinski.x360@interia.pl>
2016-06-05 22:05:24 +0200

    way from range method aka getPath

--- app/src/main/java/pl/edu/pw/elka/hexancientempires/Game.java
+++ app/src/main/java/pl/edu/pw/elka/hexancientempires/Game.java
@@ -1,5 +1,8 @@
 package pl.edu.pw.elka.hexancientempires;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Handles some game logic and other stuff
  * moreover is cool named, so no worries m8s
@@ -7,6 +10,26 @@ package pl.edu.pw.elka.hexancientempires;
  * Created by tomek on 05.06.16.
  */
 public class Game {
+    ArrayList<UnitAttackRange.Node> atakRange;
 
+    public ArrayList<Point> getPath(ArrayList<UnitRangeBFS.Node> moveRange,Point current){
+        ArrayList<Point> way = new ArrayList<>();
+        way.add(current);
 
+        while (current!= null){
+            current = moveRange.get(getIndex(moveRange,current)).parent;
+            way.add(current);
+        }
+
+        Collections.reverse(way);
+        return way;
+    }
+
+    private int getIndex(ArrayList<UnitRangeBFS.Node> moveRange,Point loc){
+        for(int i = 0; i < moveRange.size();i++ ){
+            if(moveRange.get(i).loc == loc)
+                return i;
+        }
+        throw new IllegalArgumentException("move range path is broken");
+    }
 }
